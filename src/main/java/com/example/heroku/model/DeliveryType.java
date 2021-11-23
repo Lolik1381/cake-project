dedc7fb0d9e5adf63b154f4ea3806f712b8d8c74
yuliaradzhabova <itigrenok2001@gmail.com>
2021-11-23 19:12:01 +0300

    CJ4 - Модели

---
+++ src/main/java/com/example/heroku/model/DeliveryType.java
@@ -0,0 +1,18 @@
+package com.example.heroku.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DeliveryType {
+
+    private String id;
+    private String name;
+}
